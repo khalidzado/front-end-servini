fa5dd00cd50949d16f3ee8f92d8166c30cf1a245
khalidzado <khalidzado1762004@gmail.com>
2025-11-23 01:25:35 +0100

    ajouter 2 pages login et resgister

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Progress from "../components/Progress";
+import Login from "../components/Login";
+const LoginPage = () => {
+
+  return (
+
+    <>
+      <Progress />
+
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Progress from "../components/Progress";
+import Register from "../components/Register";
+const RegisterPage = () => {
+
+  return (
+
+    <>
+      <Progress />
+
+      <Register />
+    </>
+  );
+};
+
+export default RegisterPage;
